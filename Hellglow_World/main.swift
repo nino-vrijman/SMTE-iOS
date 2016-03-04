91413837f8d0ab2fd6a6c1b0f4a26d65c6221c42
Nino Vrijman <n.vrijman@student.fontys.nl>
2016-03-04 10:59:11 +0100

    Initial Commit

---
+++ Hellglow_World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hellglow_World
+//
+//  Created by Nino Vrijman on 04/03/16.
+//  Copyright © 2016 Nino Vrijman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
